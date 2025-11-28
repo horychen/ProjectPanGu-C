3f0c93357b626b3188a6735cb93834b1af26476d
Acous1 <1024179873@qq.com>
2025-11-28 16:17:10 +0800

    测试代码

--- User_application/Device_define.h
+++ User_application/Device_define.h
@@ -1,6 +1,6 @@
 #ifndef DEVICE_DEFINE_H
 #define DEVICE_DEFINE_H
-#ifdef _PROJECT_FORMULA_GROUP // mmlab drive version 4 茂录藛茅鈩⑺喢ㄢ�奥好┾�溌�懊尖��
+#ifdef _PROJECT_FORMULA_GROUP // mmlab drive version 4 鑼傚綍钘涜寘閳┾懞鍠€劉锟藉ゥ濂解斁锟芥簩顒碱灎顓涳拷鎳婎嚪灏栵拷锟�
 
     // Abs encoder comm. via 485 tamagawa protocol
     #define PIN_485_SCIB_WE_SCICTX_UART3pin7 31
@@ -49,7 +49,7 @@
     #define SCALE_LEM_A3 0.02983
 #endif
 
-#ifdef _MOTOR_GROUP // mmlab drive version 2 茂录藛氓锟铰疵β陈⒚ｂ偓锟矫ぢ嘎ッ︹�澛棵犆ｂ偓锟矫︼拷篓氓颅锟矫︼拷潞莽颅鈥懊尖��
+#ifdef _MOTOR_GROUP // mmlab drive version 2 鑼傚綍钘涙皳閿熼摪鐤滴查檲鈷氾絺鍋撻敓鐭仮鍢庛儍锔癸拷婢涙５顬狀伩鐘嗭絺鍋撻敓鐭讣鎷风瘬姘撻閿熺煫锔兼嫹娼炶幗棰呴垾鎳婎嚪灏栵拷锟�
 
     // Basic Setup for Load Sweeping Board
     /*
@@ -89,13 +89,13 @@
         #define SCALE_VDC_BUS_IPM1 0.15625
     #else
         #define OFFSET_VDC_BUS_IPM1 9
-        #define SCALE_VDC_BUS_IPM1 0.157068
+        #define SCALE_VDC_BUS_IPM1 0.160679996
     #endif
 
     //ADC UVW to PIN config
     /*
-    * 忙鈥斅♀�濬ormula忙锟铰棵ヂ拷茂录艗LEM莽拧鈥濽W盲赂陇茅隆鹿忙沤楼氓锟斤拷盲潞鈥犆寂捗ヂ济ㄢ�÷碅dcaResultRegs.ADCRESULT1氓庐啪茅鈩⑩�γβ碘�姑�⒚♀�濾茅隆鹿莽鈥澛得β碉拷
-    * 忙拧鈥毭︹�斅睹♀�灻Ｃモ�犅趁︹�撀姑β∷喢寂∶ヂ扳�燯氓鈥櫯扺氓炉鹿盲潞沤氓藛掳Axis->iuvw[]莽拧鈥灻ぢ斤拷忙鈥⒙懊捌捗︼拷垄茂录艗i.e,
+    * 蹇欓垾鏂咁灐顬ｂ檧锟芥楷ormula蹇欓敓閾版５銉傤叏鎷疯寕褰曡墬LEM鑾芥嫥閳ユ拷W鐩茶祩闄囪寘闅嗛箍蹇欐菠妤兼皳閿熸枻鎷风洸娼為垾鐘嗩嚪瀵傛崡銉傤嚪娴庛劉锟矫风dcaResultRegs.ADCRESULT1姘撳簮鍟寘閳┾懇锟轿澄茬锟藉顭婎垪锟解挌顬ｂ檧锟芥烤鑼呴殕楣胯幗閳ユ緵寰椢茬鎷�
+    * 蹇欐嫥閳ユ锔癸拷鏂呯澒顬ｂ檧锟界伝顭婎灎锛ｃ儮锟界妳瓒侊腹锟芥拃濮懳测埛鍠㈩嚪瀵傗埗銉傛壋锟界嚡姘撻垾娅壓姘撶倝楣跨洸娼炴菠姘撹棝鎺矨xis->iuvw[]鑾芥嫥閳ョ伝銇㈡枻鎷峰繖閳モ挋鎳婎煀鎹屾崡锔兼嫹鍨勮寕褰曡墬i.e,
     * #define PIN_ADCA_U 2
     * #define PIN_ADCA_V 1
     * #define PIN_ADCA_W 0
@@ -120,7 +120,7 @@
 
     
 
-    // Lem 2莽拧鈥灻ぢ糕�懊ぢ嘎ㄢ�滐拷猫鈥奥裁ワ拷鈥斆ワ拷鈥斆ニ嗏�犆ニ喡λ溌痑dc a1 a2 a3
+    // Lem 2鑾芥嫥閳ョ伝銇㈢硶锟芥噴銇㈠槑顎€劉锟芥粣鎷风尗閳ュゥ瑁併儻鎷烽垾鏂嗐儻鎷烽垾鏂嗐儖鍡忥拷鐘嗐儖鍠☆個位婧岀棏dc a1 a2 a3
     // In fact A is the first inverter in MOTOR_GROUP
     #if BOOL_LOAD_SWEEPING_ON
         #define OFFSET_LEM_A1 2035.0 // WuBo tuned in 20241117 //2010  // 2034  // 2029.57894737 // ADCA1
@@ -137,10 +137,10 @@
         #define SCALE_LEM_A2 0.030334  // ADCA2
         #define SCALE_LEM_A3 0.031633  // ADCA3
     #else // Default Setup for XXXX (Waiting for a name as a gift for Little Black Board)
-    //LEM1-100(盲潞搂氓鈥滐拷盲禄拢氓锟铰�)
-        #define OFFSET_LEM_A1 2015 // 2035.0 // WuBo tuned in 20241027 //2010  // 2034  // 2029.57894737 // ADCA1
-        #define OFFSET_LEM_A2 2042 // 2047.0 // WuBo tuned in 20241027 //2038   // 2049  // 2043.08771930 // ADCA2
-        #define OFFSET_LEM_A3 2033 // 2057.0 // WuBo tuned in 20241027 //2029   // 2050  // 2042.98245614 // ADCA3
+    //LEM1-100(鐩叉綖鎼傛皳閳ユ粣鎷风洸绂勬嫝姘撻敓閾帮拷)
+        #define OFFSET_LEM_A1 2013 // 2035.0 // WuBo tuned in 20241027 //2010  // 2034  // 2029.57894737 // ADCA1
+        #define OFFSET_LEM_A2 2039 // 2047.0 // WuBo tuned in 20241027 //2038   // 2049  // 2043.08771930 // ADCA2
+        #define OFFSET_LEM_A3 2030 // 2057.0 // WuBo tuned in 20241027 //2029   // 2050  // 2042.98245614 // ADCA3
         #define SCALE_LEM_A1 0.0305   // 0.03080704 // ADCA1
         #define SCALE_LEM_A2 0.030334 // 0.03060669 // ADCA2
         #define SCALE_LEM_A3 0.031633  // 0.03045988 // ADCA3
@@ -190,7 +190,7 @@
     //     #define SCALE_LEM_A3 0.031633  // 0.03045988 // ADCA3
     // #endif
 
-    // Lem 1莽拧鈥灻ぢ糕�懊ぢ嘎ㄢ�滐拷猫鈥奥裁ワ拷鈥斆ワ拷鈥斆ニ嗏�犆ニ喡λ溌痑dc b7 b8 b9 // 盲禄陇茅鈧�犆ワ拷藴氓鈩⒙锯�溍モ�÷好ε掆�∶ワ拷鈥樏р�澛得ε撀好ぢ嘎好βＣ︹�撀姑ワ拷鈥樏寂捗ㄢ�孤EM盲赂艩莽拧鈥灻ヂぢ疵ぢ概矫βＣ︹�撀姑ワ拷鈥樏р�郝该ワ拷艗茂录艗氓藛鈩CALE盲赂潞忙颅拢忙鈥⒙懊寂捗ㄢ�孤EM盲赂艩莽拧鈥灻ヂぢ疵ぢ概矫βＣ︹�撀姑ワ拷鈥樏р�郝该ワ拷锟矫寂捗ニ嗏劉SCALE盲赂潞猫麓鸥忙鈥⒙懊寂�
+    // Lem 1鑾芥嫥閳ョ伝銇㈢硶锟芥噴銇㈠槑顎€劉锟芥粣鎷风尗閳ュゥ瑁併儻鎷烽垾鏂嗐儻鎷烽垾鏂嗐儖鍡忥拷鐘嗐儖鍠☆個位婧岀棏dc b7 b8 b9 // 鐩茬闄囪寘閳ь兘锟界妴銉嫹钘存皳閳┾挋顭嬵煀閿拷婧嶃儮锟矫峰ソ顬狀伩顕肝垫巻锟解埗銉嫹閳ユ◤褉锟芥緵寰椢垫拃濂姐仮鍢庡ソ尾顒伙迹锔癸拷鎾�濮戙儻鎷烽垾妯忣嚪瀵傛崡銊拷瀛ゎ檼EM鐩茶祩鑹╄幗鎷ч垾鐏活灎顔欘劶銉傘仮鐤点仮姒傜煫尾顒伙迹锔癸拷鎾�濮戙儻鎷烽垾妯徰�锟介儩璇ャ儻鎷疯墬鑼傚綍鑹楁皳钘涢埄顢笴ALE鐩茶祩娼炲繖棰呮嫝蹇欓垾鈷欐噴顕峰瘋鎹椼劉锟藉顧扙M鐩茶祩鑹╄幗鎷ч垾鐏活灎顔欘劶銉傘仮鐤点仮姒傜煫尾顒伙迹锔癸拷鎾�濮戙儻鎷烽垾妯徰�锟介儩璇ャ儻鎷烽敓鐭嚪瀵傛崡銉嬪棌鍔塖CALE鐩茶祩娼炵尗楹撻弗蹇欓垾鈷欐噴顕峰瘋锟�
     // B is the second inverter in MOTOR_GROUP
     #if BOOL_TELEOPERARION_WITH_FORMULA_BOARD
         #define OFFSET_LEM_B7 2036  // ADCB7
@@ -200,7 +200,7 @@
         #define SCALE_LEM_B8 0.03038256 // ADCB8
         #define SCALE_LEM_B9 0.03039058 // ADCB9
     #else
-    // LEM2-101(盲潞搂氓鈥滐拷盲禄拢氓锟铰�)
+    // LEM2-101(鐩叉綖鎼傛皳閳ユ粣鎷风洸绂勬嫝姘撻敓閾帮拷)
         #define OFFSET_LEM_B7 2012 //2020 // 2023.89473684 // ADCB7
         #define OFFSET_LEM_B8 2042 // 2029 // 2042.33333333 // ADCB8
         #define OFFSET_LEM_B9 2033 // 2038 // 2043.43859649 // ADCB9
@@ -210,7 +210,7 @@
     #endif
 
 #endif
-#ifdef _INDUCTION_MOTOR_GROUP // mmlab drive version 3 茂录藛莽沤鈥姑ワ拷茠莽颅鈥懊尖��
+#ifdef _INDUCTION_MOTOR_GROUP // mmlab drive version 3 鑼傚綍钘涜幗娌ら垾濮戙儻鎷疯尃鑾介閳ユ噴顕峰皷锟斤拷
 
     // Abs encoder comm. via 485 tamagawa protocol
     #define PIN_485_SCIB_WE_SCICTX_UART3pin7 31
@@ -270,7 +270,7 @@
 
 #endif
 
-#ifdef _LEG_GROUP // _LEG_GROUP // mmlab drive version 1 (茅鈩⑺喢ㄢ�奥好┾�溌ｂ偓锟矫ε撀泵ぢ颗犆Ｅ犆ｂ偓锟矫ㄆ捖∶р�樷偓莽颅鈥�)
+#ifdef _LEG_GROUP // _LEG_GROUP // mmlab drive version 1 (鑼呴埄鈶哄枹銊拷濂ュソ鈹撅拷婧岊劶锝傚亾閿熺煫蔚鎾�娉点仮棰楃妴顬狅讥鐘嗭絺鍋撻敓鐭剢鎹栤埗褉锟芥ǚ鍋撹幗棰呴垾锟�)
 
     // Abs encoder comm. via 485 tamagawa protocol
     #define PIN_485_SCIB_WE_SCICTX_UART3pin7 140
@@ -308,7 +308,7 @@
     #define PIN_ADCB_V 4
     #define PIN_ADCB_W 5
 
-    // Lem 1莽拧鈥灻ぢ糕�懊ぢ嘎ㄢ�滐拷猫鈥奥裁ワ拷鈥斆ワ拷鈥斆ニ嗏�犆ニ喡λ溌痑dc b7 b8 b9 // 盲禄陇茅鈧�犆ワ拷藴氓鈩⒙锯�溍モ�÷好ε掆�∶ワ拷鈥樏р�澛得ε撀好ぢ嘎好βＣ︹�撀姑ワ拷鈥樏寂捗ㄢ�孤EM盲赂艩莽拧鈥灻ヂぢ疵ぢ概矫βＣ︹�撀姑ワ拷鈥樏р�郝该ワ拷艗茂录艗氓藛鈩CALE盲赂潞忙颅拢忙鈥⒙懊寂捗ㄢ�孤EM盲赂艩莽拧鈥灻ヂぢ疵ぢ概矫βＣ︹�撀姑ワ拷鈥樏р�郝该ワ拷锟矫寂捗ニ嗏劉SCALE盲赂潞猫麓鸥忙鈥⒙懊寂�
+    // Lem 1鑾芥嫥閳ョ伝銇㈢硶锟芥噴銇㈠槑顎€劉锟芥粣鎷风尗閳ュゥ瑁併儻鎷烽垾鏂嗐儻鎷烽垾鏂嗐儖鍡忥拷鐘嗐儖鍠☆個位婧岀棏dc b7 b8 b9 // 鐩茬闄囪寘閳ь兘锟界妴銉嫹钘存皳閳┾挋顭嬵煀閿拷婧嶃儮锟矫峰ソ顬狀伩顕肝垫巻锟解埗銉嫹閳ユ◤褉锟芥緵寰椢垫拃濂姐仮鍢庡ソ尾顒伙迹锔癸拷鎾�濮戙儻鎷烽垾妯忣嚪瀵傛崡銊拷瀛ゎ檼EM鐩茶祩鑹╄幗鎷ч垾鐏活灎顔欘劶銉傘仮鐤点仮姒傜煫尾顒伙迹锔癸拷鎾�濮戙儻鎷烽垾妯徰�锟介儩璇ャ儻鎷疯墬鑼傚綍鑹楁皳钘涢埄顢笴ALE鐩茶祩娼炲繖棰呮嫝蹇欓垾鈷欐噴顕峰瘋鎹椼劉锟藉顧扙M鐩茶祩鑹╄幗鎷ч垾鐏活灎顔欘劶銉傘仮鐤点仮姒傜煫尾顒伙迹锔癸拷鎾�濮戙儻鎷烽垾妯徰�锟介儩璇ャ儻鎷烽敓鐭嚪瀵傛崡銉嬪棌鍔塖CALE鐩茶祩娼炵尗楹撻弗蹇欓垾鈷欐噴顕峰瘋锟�
     #define OFFSET_LEM_B7 2025 // 2023.89473684 // ADCB7
     #define OFFSET_LEM_B8 2041 // 2042.33333333 // ADCB8
     #define OFFSET_LEM_B9 2045 // 2043.43859649 // ADCB9
@@ -316,7 +316,7 @@
     #define SCALE_LEM_B8 0.03038256 // ADCB8
     #define SCALE_LEM_B9 0.03039058 // ADCB9
 
-    // Lem 2莽拧鈥灻ぢ糕�懊ぢ嘎ㄢ�滐拷猫鈥奥裁ワ拷鈥斆ワ拷鈥斆ニ嗏�犆ニ喡λ溌痑dc a1 a2 a3
+    // Lem 2鑾芥嫥閳ョ伝銇㈢硶锟芥噴銇㈠槑顎€劉锟芥粣鎷风尗閳ュゥ瑁併儻鎷烽垾鏂嗐儻鎷烽垾鏂嗐儖鍡忥拷鐘嗐儖鍠☆個位婧岀棏dc a1 a2 a3
     #define OFFSET_LEM_A1 2030      // 2029.57894737 // ADCA1
     #define OFFSET_LEM_A2 2043      // 2043.08771930 // ADCA2
     #define OFFSET_LEM_A3 2042      // 2042.98245614 // ADCA3
@@ -330,7 +330,7 @@
 
 #if 0 // Device Lib
 
-    //LEM1-100(盲潞搂氓鈥滐拷盲禄拢氓锟铰�)
+    //LEM1-100(鐩叉綖鎼傛皳閳ユ粣鎷风洸绂勬嫝姘撻敓閾帮拷)
         #define OFFSET_LEM_B7 2020 // 2023.89473684 // ADCB7
         #define OFFSET_LEM_B8 2029 // 2042.33333333 // ADCB8
         #define OFFSET_LEM_B9 2038 // 2043.43859649 // ADCB9
