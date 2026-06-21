f30941f20d340725ad3772b859672c715648f1e5
Acous1 <1024179873@qq.com>
2026-06-21 22:48:51 +0800

    标定传感器

--- User_application/ACMSim.h
+++ User_application/ACMSim.h
@@ -153,7 +153,7 @@ REAL difference_between_two_angles(REAL first, REAL second);
 #define CNT_2_ELEC_RAD (SYSTEM_QEP_REV_PER_PULSE * 2 * M_PI * d_sim.init.npp)
 #define SYSTEM_QEP_QPOSMAX (9999)
 #define SYSTEM_QEP_QPOSMAX_PLUS_1 (10000)
-#define OFFSET_COUNT_BETWEEN_ENCODER_INDEX_AND_U_PHASE_AXIS 8881// wb tuned with id=3A 20250930 // wb tuned with id=3A 20241027 // 2353 // cjh tuned with id_cmd = 4A @2024-10-05 // This value need an average value due to XXX(CJH told me one day but here space is not enough to print it out)
+#define OFFSET_COUNT_BETWEEN_ENCODER_INDEX_AND_U_PHASE_AXIS 7865// wb tuned with id=3A 20250930 // wb tuned with id=3A 20241027 // 2353 // cjh tuned with id_cmd = 4A @2024-10-05 // This value need an average value due to XXX(CJH told me one day but here space is not enough to print it out)
 #define positive_current_QPOSCNT_counting_down (-1) // 正向旋转的电流导致增量式编码器QEP读数减少 则填 -1，否则默认为 1。
 
 
